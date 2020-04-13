212a9820574d0b8f06e99a100bf7c107c7eef5fb
SVista <svista@svista.ddns.net>
2020-04-13 18:46:46 +0100

    First push!!!

---
+++ ocarina.bat
@@ -0,0 +1,1 @@
+python %userprofile%\ocarina\ocarina.py %1 %2
---
+++ ocarina.py
@@ -0,0 +1,24 @@
+import colored as coloured
+import sys
+import ocarina_api
+
+def getRepo():
+    f = open(ocarina_api.getUserDir(True)+'/ocarina/repo.txt','r')
+    repo = f.read()
+    f.close()
+    return repo
+
+try:
+    if sys.argv[1] == 'get':
+        ocarina_api.getAppInfoFromRepo(sys.argv[2],getRepo())
+    elif sys.argv[1] == 'set-active-repo':
+        if sys.argv[2] == 'official':
+            sys.argv[2] = 'https://raw.githubusercontent.com/StonyVista/ocarina-official-repo/master'
+        f = open(ocarina_api.getUserDir(True)+'/ocarina/repo.txt','w')
+        f.write(sys.argv[2])
+        f.close()
+    else:
+        print(coloured.fg('#ff0000')+'ocarina front end error: command: '+sys.argv[1]+'does not exist.'+coloured.attr('reset'))
+except IndexError:
+    print(coloured.fg('#ff0000')+'ocarina front end error: How about typing a command next time??'+coloured.attr('reset'))
+    print('Commands:\nget: gets app with supplied name EG: python ocarina.py get firefox\nset-active-repo sets the active repository EG: python ocarina.py set-active-repo official (also works with repository urls)')
