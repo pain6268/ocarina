b852dafc460cc0ff86fdd922a50b7751653838de
SVista <svista@svista.ddns.net>
2020-04-13 19:23:21 +0100

    removed old installer

--- installer/ocarina installer.bat
+++
@@ -1,7 +0,0 @@
-echo Installing ocarina... please wait a sec
-mkdir %userprofile%\ocarina
-copy ocarina.py %userprofile%\ocarina\ocarina.py
-copy ocarina_api.py %userprofile%\ocarina\ocarina_api.py
-copy repo.txt %userprofile%\ocarina\repo.txt
-copy ocarina.bat %windir%\system32\ocarina.bat
-echo Done! Enjoy using ocarina!
