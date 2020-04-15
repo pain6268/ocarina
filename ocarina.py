d4c857f6b4794585f559f6cae161e0997ce50a27
SVista <svista@svista.ddns.net>
2020-04-15 18:37:48 +0100

    Alpha 1.1 (YAY!!!)

--- ocarina.py
+++ ocarina.py
@@ -1,4 +1,3 @@
-import colored as coloured
 import sys
 import ocarina_api
 
@@ -10,7 +9,35 @@ def getRepo():
 
 try:
     if sys.argv[1] == 'get':
-        ocarina_api.getAppInfoFromRepo(sys.argv[2],getRepo())
+        print(ocarina_api.blue255+'Hang on a sec... getting app info...')
+        ocarina_api.getAppInfoFromRepo(sys.argv[2],getRepo()) #Downloads the app-info file from the ocarina repository
+        if ocarina_api.functionRanCorrectly != True:
+            print(ocarina_api.red255+'Error (2): Could not find "'+sys.argv[2]+'" please make sure that this program actually exists and that you are typing the name correctly'+ocarina_api.endcolour)
+            exit()
+        print(ocarina_api.green255+'Info found!'+ocarina_api.endcolour)
+        appData = ocarina_api.readDataFromInfoFile(sys.argv[2])
+        if ocarina_api.functionRanCorrectly != True:
+            print(ocarina_api.red255+'Error (3): There is a problem with the repository please contact the repo admin.'+ocarina_api.endcolour)
+            exit()
+        print(ocarina_api.green255+'Info extracted!'+ocarina_api.endcolour)
+        if appData[1] == 'console-out':
+            print(ocarina_api.blue255+appData[2]+ocarina_api.endcolour)
+            exit()
+        else:
+            verify = input(ocarina_api.blue255+'Do you want to download '+appData[0]+'? (n/Y):'+ocarina_api.endcolour)
+            appExtension = ocarina_api.getAppExecutable(sys.argv[2],appData[0],appData[2])
+        print(ocarina_api.blue255+'Data downloaded for '+appData[0]+ocarina_api.endcolour)
+        if appData[1] == 'app-ins':
+            print(ocarina_api.blue255+'ocarina will now execute the setup file...'+ocarina_api.endcolour)
+            ocarina_api.executeSetupFile(sys.argv[2],appExtension)
+            verify = input(ocarina_api.blue255+'Do you want to delete the setup file? (n/Y): '+ocarina_api.endcolour)
+            if verify == True:
+                ocarina_api.os.remove(ocarina_api.getUserDir()+'/appdata/'+sys.argv[2]+'/app.'+appExtension)
+        elif appData[1] == 'prg-zip':
+            print(ocarina_api.blue255+'Extracting...'+ocarina_api.endcolour)
+            ocarina_api.unzipZippedFile(sys.argv[2])
+            ocarina_api.executeExe(appData[3])
+
     elif sys.argv[1] == 'set-active-repo':
         if sys.argv[2] == 'official':
             repo = 'https://raw.githubusercontent.com/StonyVista/ocarina-official-repo/master'
@@ -19,8 +46,14 @@ try:
         f = open(ocarina_api.getUserDir()+'/repo.txt','w')
         f.write(repo)
         f.close()
+    elif sys.argv[1] = 'launch':
+        print(ocarina_api.blue255+'Launching...'+ocarina_api.endcolour)
+        try:
+            ocarina_api.executeExe(sys.argv[2],ocarina_api.readDataFromInfoFile(sys.argv[2])[3])
+        except IndexError:
+            print(ocarina_api.red255+'Error (4): Selected app is most likely nat an exe from a zip folder. Launch it from the start menu')
     else:
-        print(coloured.fg('#ff0000')+'ocarina front end error: command: '+sys.argv[1]+'does not exist.'+coloured.attr('reset'))
+        print(ocarina_api.red255+'ocarina front end error: command: '+sys.argv[1]+'does not exist.'+ocarina_api.endcolour)
 except IndexError:
-    print(coloured.fg('#ff0000')+'ocarina front end error: How about typing a command next time??'+coloured.attr('reset'))
-    print('Commands:\nget: gets app with supplied name EG: python ocarina.py get firefox\nset-active-repo sets the active repository EG: python ocarina.py set-active-repo official (also works with repository urls)')
+    print(ocarina_api.red255+'ocarina front end error: How about typing a command next time??'+ocarina_api.endcolour)
+    print('Commands:\nget: gets app with supplied name EG: ocarina get firefox\nset-active-repo sets the active repository EG: ocarina set-active-repo official (also works with repository urls)\nlaunch: Launches app from zipped folder (app must be downloaded with ocarina first) EG: ocarina launch <appnamehere.')
--- ocarina_api.py
+++ ocarina_api.py
@@ -3,14 +3,16 @@ import os
 import colored as coloured
 import urllib.request
 import sys
+import zipfile
 
 global verifiedSystem
+global functionRanCorrectly
 verifiedSystem = False
 
 endcolour = coloured.attr('reset') #resets string colour. Example below --------------------------------V
 red255 = coloured.fg('#ff0000') #red255 before a string in a print makes the rext #ff0000 (255 red) EG: print(red255+'insert text here'+endcolour)
-blue255 = coloured.fg('#0000ff') #ditto but for 255 blue
-green255 = coloured.fg('#00ff00') #ditto but for 255 green
+blue255 = coloured.fg('#3969fa') #ditto but for 255 blue
+green255 = coloured.fg('#39b336') #ditto but for 255 green
 
 def baseSystemCheck(): #Checks if ocarina is running on Windows or not
     if os.name != 'nt':
@@ -25,9 +27,9 @@ def dlFile(remoteAddress, localAddress):
     if verifiedSystem == True:
         try:
             urllib.request.urlretrieve(remoteAddress, localAddress) #downloads file from remoteAddress (location on web) to localAddress (location on this computer) eg: remote: https://svista.ddns.net/ocarina.py local: C:/Users/Administrator/ocarina.py
-            return 1 #Download success
+            return True #Download success
         except urllib.error.HTTPError:
-            return 0 #Download fail
+            return False #Download fail
     else:
         print(red255+'Error (1): ocarina has detected you are not running Windows.\nIf you are wanting to test ocarina, please use Windows.'+endcolour)
 
@@ -42,67 +44,50 @@ def genFolder(path):
         os.mkdir(getUserDir()+'/'+path)
     return path
 
-def getAppExecutable(basename, name, type, url): #type is unused for now
-    if verifiedSystem == True:
-        verify = input(blue255+'Would you like to download '+name+'? (n/Y): '+endcolour)
-        if verify.lower() != 'n':
-            appDLLocation = getUserDir()+'/'+genFolder('appdata/'+basename)+'/app.'+url.split('.')[-1]
-            print(blue255+'Now downloading '+name+' from its defined source ('+url+')...'+endcolour)
-            dlStatus = dlFile(url,appDLLocation)
-            if dlStatus == 1:
-                print(green255+'Data downloaded successfully...'+endcolour)
-                print(blue255+'ocarina will now execute the setup file...')
-                os.system(appDLLocation)
-                verify = input(blue255+'Do you want to delete the setup files? (n/Y): '+endcolour)
-                if verify.lower() != 'n':
-                    os.remove(appDLLocation)
-                    print(green255+'Done!'+endcolour)  
-            else:
-                print(red255+'Error (4): Could not find the files at the defined source'+endcolour)
-                p
-    else:
-        print(red255+'Error (1): ocarina has detected you are not running Windows.\nIf you are wanting to test ocarina, please use Windows.'+endcolour)
+def getAppExecutable(basename, name, url):
+    global functionRanCorrectly
+    appDLLocation = getUserDir()+'/'+genFolder('appdata/'+basename)+'/app.'+url.split('.')[-1]
+    functionRanCorrectly = dlFile(url,appDLLocation)
+    return url.split('.')[-1]
+
+def executeSetupFile(appName,extension):
+    os.system(getUserDir()+'/appdata/'+appName+'/app.'+extension)
+
+def executeExe(appName,exeLocation):
+    os.system(getUserDir()+'/appdata/'+appName+'/'+exeLocation)
+
+def unzipZippedFile(appName):
+    archive = zipfile.ZipFile(getUserDir()+'/appdata/'+appName+'/app.zip')
+    archive.extractall()
+    archive.close()
 
 def getAppInfoFromRepo(appName,repository):
-    if verifiedSystem == True:
-        appDLLocation = getUserDir()+'/'+genFolder('appdata/'+appName)+'/app.ocarina-info'
-        print(blue255+'Getting info for '+appName+' from the repository...'+endcolour)
-        dlStatus = dlFile(repository+'/apps/'+appName+'/app.ocarina-info',appDLLocation) #this line downloads the app info file for the selected app on the active repository
-        if dlStatus == 1:
-            print(green255+'Info successfully downloaded from the repository!'+endcolour)
-            print(blue255+'Decoding info file...'+endcolour)
-            f = open(appDLLocation,'r') #opening data file
-            appData = f.readlines() # reading data file to var
-            f.close() #closing data file
-            for line in range(0,3): #formatting file into somrthing readable by the program
-                appData[line] = appData[line].rstrip().split('*') #splits the data into a list containing each line broken down into a list where the colon seperated them
-            dlFileProblematic = False
-            for entry in appData:
-                if entry[0] == 'name':
-                    appFullName = entry[1]
-                elif entry[0] == 'type':
-                    dlType = entry[1]
-                elif entry[0] == 'url':
-                    dlUrl = entry[1]
-                else:
-                    print(red255+'Error (3): There is something wrong with the repository, it might be broken or be designed for a newer version of ocarina. Please check your version or contact the Repository Admin.'+endcolour)
-                    print(appData)
-                    if repository != 'https://raw.githubusercontent.com/StonyVista/ocarina-official-repo/master':
-                        print(red255+'Note: you are not using the official repository. Therefore the anly support that can be given is to contact the Repository Admin since this cannot be an issue with ocarina itself.'+endcolour)
-                    else:
-                        print(red255+'If you believe this is an issue with the official repository, please post an issue here: https://github.com/StonyVista/ocarina-official-repo/issues'+endcolour)
-                    dlFileProblematic = True #Raises to the program that there's a problem with the info file
-            if dlFileProblematic == False:
-                print(green255+'Yay! The info file was successfully decoded! ocarina is now able to download the data for '+appFullName+'.')
-                getAppExecutable(appName,appFullName, dlType, dlUrl)  
-        else:
-            print(red255+'Error (2): Could not find data for "'+appName+'" on the repository. Please make sure the name is typed correctly and that the program you are trying to get is ACTUALLY on the repository.'+endcolour)
-            if repository != 'https://raw.githubusercontent.com/StonyVista/ocarina-official-repo/master':
-                print(red255+'Furthermore, it has been detected that you are not using the official ocarina repository. Please make sure that the repository you are using is configured correctly. If not, contact the Repository Admin.\n(Note: you will not be able to recive support for unnofficial repositories as SVista has no control over them.)'+endcolour)
-            else:
-                print('If you believe this is an issue with the official repository, please post an issue here: https://github.com/StonyVista/ocarina-official-repo/issues')
-    else:
-        print(red255+'Error (1): ocarina has detected you are not running Windows.\nIf you are wanting to test ocarina, please use Windows.'+endcolour)
+    global functionRanCorrectly
+    appDLLocation = getUserDir()+'/'+genFolder('appdata/'+appName)+'/app.ocarina-info'
+    functionRanCorrectly = dlFile(repository+'/apps/'+appName+'/app.ocarina-info',appDLLocation) #this line downloads the app info file for the selected app on the active repository
 
-print(green255+'Thanks for using ocarina!!'+endcolour)
+def readDataFromInfoFile(appName): 
+    global functionRanCorrectly
+    f = open(getUserDir()+'/appdata/'+appName+'/app.ocarina-info','r')
+    appData = f.readlines()
+    f.close()
+    for line in range(0,len(appData)):
+                appData[line] = appData[line].rstrip().split('*') #splits the data into a list containing each line broken down into a list where the colon seperated them
+    exeInZip = 'none'
+    for entry in appData:
+        if entry[0] == 'name':
+            appFullName = entry[1]
+        elif entry[0] == 'type':
+            dlType = entry[1]
+        elif entry[0] == 'url': #URL shouldn't be used in info files anymore... kept here for backwards compatibility
+            dlData = entry[1]
+        elif entry[0] == 'data':
+            dlData = entry[1]
+        elif entry[0] == 'zip-exe':
+            exeInZip = entry[1]
+        else:
+            functionRanCorrectly = False
+    return [appFullName, dlType, dlData, exeInZip]
+
+print(green255+'Thanks for using ocarina!! (alpha 1.1 progress version)'+endcolour)
 genFolder('appdata')
